9bd65162e2b85bb2627f810d68379097c6f12d5c
Jack Chen <massd.me@gmail.com>
2013-04-25 23:54:21 +0100

    modified element array to element list. finished encoding int32, int64 and float. need to continue to finish the encoding of doc

--- src/ocaml_bson.ml
+++ src/ocaml_bson.ml
@@ -13,7 +13,7 @@ and
   | Double of float
   | String of string
   | Document of document
-  | Array of document array
+  | Array of document list
   | Binary of binary
   | ObjectId of string (* only 12 bytes *)
   | Boolean of bool
@@ -29,11 +29,11 @@ and
   | MaxKey
 and
   binary =
-  | Generic of byte array
-  | Function of byte array
-  | UUID of byte array
-  | MD5 of byte array
-  | UserDefined of byte array;;
+  | Generic of Buffer.t
+  | Function of Buffer.t
+  | UUID of Buffer.t
+  | MD5 of Buffer.t
+  | UserDefined of Buffer.t;;
 
 exception Bson_invalid_objectId;;
 
@@ -47,7 +47,14 @@ let put ename e doc = StringMap.add ename e doc;;
 let put_double ename v doc = put ename (Double v) doc;;
 let put_string ename v doc = put ename (String v) doc;;
 let put_doc ename v doc = put ename (Document v) doc;;
-let put_array ename v doc = put ename (Array v) doc;;
+let put_list ename v doc = 
+  let rec build_doc_list i acc = function 
+    | [] -> acc 
+    | hd::tl -> 
+      let new_doc = put (String.make 1 (Char.chr (i+48))) hd (create_doc()) in
+      build_doc_list (i+1) (new_doc::acc) tl 
+  in 
+  put ename (List.rev (build_doc_list 0 [] v)) doc;;
 let put_generic_binary ename v doc = put ename (Binary (Generic v)) doc;;
 let put_function_binary ename v doc = put ename (Binary (Function v)) doc;;
 let put_uuid_binary ename v doc = put ename (Binary (UUID v)) doc;;
@@ -83,7 +90,18 @@ let get extract ename doc =
 let get_double ename doc = get (function (Double v) -> v | _ -> raise Wrong_bson_type) ename doc;;
 let get_string ename doc = get (function (String v) -> v | _ -> raise Wrong_bson_type) ename doc;;
 let get_doc ename doc = get (function (Document v) -> v | _ -> raise Wrong_bson_type) ename doc;;
-let get_array ename doc = get (function (Array v) -> v | _ -> raise Wrong_bson_type) ename doc;;
+let get_list ename doc = 
+  let doc_list = get (function (Array v) -> v | _ -> raise Wrong_bson_type) ename doc in
+  match doc_list with
+    | None -> None
+    | Some dl -> 
+      let rec build_element_list i acc = function
+	| [] -> acc
+	| hd::tl ->
+	  let element = StringMap.find (String.make 1 (Char.chr (i+48))) hd in
+	  build_element_list (i+1) (element::acc) tl
+      in 
+      Some (List.rev (build_element_list 0 [] dl));;
 let get_generic_binary ename doc = get (function (Generic v) -> v | _ -> raise Wrong_bson_type) ename doc;;
 let get_function_binary ename doc = get (function (Function v) -> v | _ -> raise Wrong_bson_type) ename doc;;
 let get_uuid_binary ename doc = get (function (UUID v) -> v | _ -> raise Wrong_bson_type) ename doc;;
@@ -102,23 +120,60 @@ let get_timestamp ename doc = get (function (Timestamp v) -> v | _ -> raise Wron
 let get_minkey ename doc = get (function MinKey -> MinKey | _ -> raise Wrong_bson_type) ename doc;;
 let get_maxkey ename doc = get (function MaxKey -> MaxKey | _ -> raise Wrong_bson_type) ename doc;;
 
+(*let encode_int64 v =
+  let rec convert i acc = 
+    if i > 7 then acc
+    else 
+      let b = Int64.logand 255L (Int64.shift_right v (i*8)) in
+      let acc_move = Int64.shift_left acc (i*8) in
+      convert (i+1) (Int64.logor acc_move b)
+  in 
+  convert 0 0L;;*)
+
 let encode_int64 v =
   let buf = Buffer.create 8 in
-  for i = 0 to 7 do 
-    Buffer.add_char buf (Int64.logand 255L (Int64.shift_right v (i*8))) 
-  done;;
+  for i = 0 to 7 do
+    let b = Int64.logand 255L (Int64.shift_right v (i*8)) in
+    Buffer.add_char buf (Char.chr (Int64.to_int b)) ;
+  done;
+  buf;;
 
 let encode_float v = encode_int64 (Int64.bits_of_float v);;
 
- 
+let encode_int32 v = 
+  let buf = Buffer.create 4 in
+  for i = 0 to 3 do
+    let b = Int32.logand 255l (Int32.shift_right v (i*8)) in
+    Buffer.add_char buf (Char.chr (Int32.to_int b)) ;
+  done;
+  buf;;
 
 (*
 let encode doc =
-  let rec encode_doc = 
+  let add_ename c ename = 
+    let buf = Buffer.create 16 in 
+    Buffer.add_char buf c; Buffer.add_string buf ename; Buffer.add_char buf '\x00';
+    buf
+  in 
+  let rec encode_doc doc = 
     let encode_element ename element = 
-      let append_end buf = Buffer.add_char buf '\x00' in
       let buf = Buffer.create 16 in
       match element with
 	| Double v -> 
-	  Buffer.add_char buf '\x01'; Buffer.add_string buf ename; append_end buf; 
-	| String -> *)
+	  Buffer.add_buffer buf (add_ename '\x01' ename);
+	  Buffer.add_buffer buf (encode_float v)
+	| String v -> 
+	  Buffer.add_buffer buf (add_ename '\x02' ename);
+	  Buffer.add_buffer buf (encode_int32 (Int32.of_int (String.length v)));
+	  Buffer.add_string buf v;
+	  Buffer.add_char '\x00'
+	| Document v -> 
+	  Buffer.add_buffer buf (add_ename '\x03' ename);
+	  Buffer.add_buffer buf (encode_doc v)
+	| Array v ->
+	  Buffer.add_buffer buf (add_ename '\x04' ename);
+	  for i = 0 to ((Array.length v)-1) do
+	    Buffer.add_buffer buf (encode_element (Char.chr (i+48)) v.(i))
+	  done
+	| Binary (Generic v) ->
+	  Buffer.add_buffer buf (add_ename '\x05' ename);*)
