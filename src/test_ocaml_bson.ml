2d3c17b7fe75befb66ab6e81dc3243dc6a780c6a
MassD <massd.me@gmail.com>
2013-04-30 18:58:17 +0100

    split module into mli and ml, and rename the module to bson, and added ocamldoc

--- src/test_ocaml_bson.ml
+++ src/test_ocaml_bson.ml
@@ -1,5 +1,3 @@
-open Ocaml_bson;;
-
 let print_buffer s = 
   String.iter (fun c -> let i = Char.code c in if i < 10 then Printf.printf "\\x0%X" i else Printf.printf "\\x%X" i) s;
   print_endline "";;
@@ -80,7 +78,7 @@ let doc21 = Bson.add_element "MaxKey" e21 doc20;;
 (*let _ = print_endline "{\"hello\":\"world\"}";;*)
 let e22 = Bson.create_string "world";;
 let doc22 = Bson.add_element "hello" e22 doc0;;
-let _ = print_endline (Bson.to_json doc22);;
+let _ = print_endline (Bson.to_simple_json doc22);;
 
 let _ = print_endline "encoding...";;
 let doc22_buf = Bson.encode doc22;;
@@ -88,7 +86,7 @@ let _ = print_buffer doc22_buf;;
 
 let _ = print_endline "decoding...";;
 let doc22_rev = Bson.decode doc22_buf;;
-let _ = print_endline (Bson.to_json doc22_rev);;
+let _ = print_endline (Bson.to_simple_json doc22_rev);;
 
 
 
@@ -98,7 +96,7 @@ print_endline "";;
 let l = [(Bson.create_string "awesome"); (Bson.create_double 5.05); (Bson.create_int32 1986l)];;
 let e23 = Bson.create_list l;;
 let doc23 = Bson.add_element "BSON" e23 doc0;;
-let _ = print_endline (Bson.to_json doc23);;
+let _ = print_endline (Bson.to_simple_json doc23);;
 
 let _ = print_endline "encoding...";;
 let doc23_buf = Bson.encode doc23;;
@@ -106,7 +104,7 @@ let _ = print_buffer doc23_buf;;
 
 let _ = print_endline "decoding...";;
 let doc23_rev = Bson.decode doc23_buf;;
-let _ = print_endline (Bson.to_json doc23_rev);;
+let _ = print_endline (Bson.to_simple_json doc23_rev);;
 
 print_endline "";;
 
