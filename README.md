c4324b02922553f1433ca27dc1cd2b56fdfb1203
Jack Chen <massd.me@gmail.com>
2013-04-30 21:56:40 +0100

    Version 0.88.0

--- README.md
+++ README.md
@@ -3,16 +3,16 @@ Bson.ml
 
 This module includes a Bson document data structure, together with its encoding (to bytes) and decoding (from bytes). 
 
-    The logic of {b usage} is like this
-    - Create an empty Bson document
-    - Create the elements you want
-    - Add elements to the document with names
-    - Or remove elements from the document via the names
-    - Get elements from the document via the names
-    - After obtaining an element, get the raw value from the element
+The logic of {b usage} is like this
+- Create an empty Bson document
+- Create the elements you want
+- Add elements to the document with names
+- Or remove elements from the document via the names
+- Get elements from the document via the names
+- After obtaining an element, get the raw value from the element
 
-    The functions inside this module seem to be many, however, most of them are just for creating elements. These functions are to {e hide the implementation details of the type elements}. Also, in this way, the Bson document can be used more safely.
+The functions inside this module seem to be many, however, most of them are just for creating elements. These functions are to {e hide the implementation details of the type elements}. Also, in this way, the Bson document can be used more safely.
 
-    Please refer to the {{: http://bsonspec.org/#/specification } Official Bson specification } for more information.
+Please refer to the {{: http://bsonspec.org/#/specification } Official Bson specification } for more information.
 
-    {e Version 0.88.0} 
+{e Version 0.88.0} 
--- test/test_ocaml_bson.ml
+++ test/test_ocaml_bson.ml
@@ -2,8 +2,6 @@ let print_buffer s =
   String.iter (fun c -> let i = Char.code c in if i < 10 then Printf.printf "\\x0%X" i else Printf.printf "\\x%X" i) s;
   print_endline "";;
 
-
-
 let bin = "12345";;
 
 let doc0 = Bson.make ();;
@@ -21,26 +19,11 @@ let doc3 = Bson.add_element "Document" e3 doc2;;
 let e4 = Bson.create_list [e1;e2;e3];;
 let doc4 = Bson.add_element "Array" e4 doc3;;
 
-(*let e5 = Bson.create_generic_binary bin;;
-let doc5 = Bson.add_element "Generic Binary" e5 doc4;;
-
-let e6 = Bson.create_function_binary bin;;
-let doc6 = Bson.add_element "Function Binary" e6 doc5
-
-let e7 = Bson.create_uuid_binary bin;;
-let doc7 = Bson.add_element "UUID Binary" e7 doc6;;
-
-let e8 = Bson.create_md5_binary bin;;
-let doc8 = Bson.add_element "MD5 Binary" e8 doc7;;*)
-
 let e9 = Bson.create_user_binary bin;;
 let doc9 = Bson.add_element "User Defined Binary" e9 doc4;;
 
 let e10 = Bson.create_objectId "123456789123";;
 let doc10 = Bson.add_element "ObjectId" e10 doc9;;
-
-(*let e10_1 = Bson.create_objectId "00123456789123";;
-let doc10_1 = Bson.add_element "ObjectId" e10_1 doc9;;*)
 
 let e11 = Bson.create_boolean true;;
 let doc11 = Bson.add_element "Boolean" e11 doc10;;
@@ -75,7 +58,6 @@ let doc20 = Bson.add_element "MinKey" e20 doc18;;
 let e21 = Bson.create_maxkey ();;
 let doc21 = Bson.add_element "MaxKey" e21 doc20;;
 
-(*let _ = print_endline "{\"hello\":\"world\"}";;*)
 let e22 = Bson.create_string "world";;
 let doc22 = Bson.add_element "hello" e22 doc0;;
 let _ = print_endline (Bson.to_simple_json doc22);;
@@ -88,11 +70,8 @@ let _ = print_endline "decoding...";;
 let doc22_rev = Bson.decode doc22_buf;;
 let _ = print_endline (Bson.to_simple_json doc22_rev);;
 
-
-
 print_endline "";;
 
-(*let _ = print_endline "{\"BSON\":[\"awesome\", 5.05, 1986]}";;*)
 let l = [(Bson.create_string "awesome"); (Bson.create_double 5.05); (Bson.create_int32 1986l)];;
 let e23 = Bson.create_list l;;
 let doc23 = Bson.add_element "BSON" e23 doc0;;
@@ -107,7 +86,3 @@ let doc23_rev = Bson.decode doc23_buf;;
 let _ = print_endline (Bson.to_simple_json doc23_rev);;
 
 print_endline "";;
-
-(*
-let doc21_buf = Bson.encode doc21;;
-let _ = print_buffer doc21_buf;;*)
