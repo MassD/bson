444d2c3b33f98c448512d36f8d32735db6742191
Jack Chen <massd.me@gmail.com>
2013-04-30 21:40:55 +0100

    Version 0.88.0

--- src/bson.mli
+++ src/bson.mli
@@ -1,26 +1,69 @@
+(** 
+    This module includes a Bson document data structure, together with its encoding (to bytes) and decoding (from bytes). 
+
+    The logic of {b usage} is like this
+    - Create an empty Bson document
+    - Create the elements you want
+    - Add elements to the document with names
+    - Or remove elements from the document via the names
+    - Get elements from the document via the names
+    - After obtaining an element, get the raw value from the element
+
+    The functions inside this module seem to be many, however, most of them are just for creating elements. These functions are to {e hide the implementation details of the type elements}. Also, in this way, the Bson document can be used more safely.
+
+    Please refer to the {{: http://bsonspec.org/#/specification } Official Bson specification } for more information.
+
+    {e Version 0.88.0} 
+*)
+
+(** Raised when an objectId's length is not 12. see http://bsonspec.org/#/specification *)
 exception Invalid_objectId;;
+
+(** Raised when an unkown bson type is met while encoding the bson doc *)
 exception Wrong_bson_type;;
+
+(** Raised only when trying to decode the bytes to string. *)
 exception Wrong_string;;
+
+(** Raised when bad things happen while decoding the bytes to bson doc *)
 exception Malformed_bson;;
 
+(** The type for the Bson document. This is the main data structure *)
 type t;;
+
+(** The type for representing the special fields in Bson *)
 type empty = 
   | NULL 
   | MINKEY
   | MAXKEY;;
+
+(** The type for the fields for the Bson document *)
 type element;;
 
-(** create an emapty bson doc *)
+
+(** {6 Basic operations on Bson document} *)
+
+(** Create an empty Bson document *)
 val make : unit -> t;;
 
+(** Create an empty Bson document *)
 val add_element : string -> element -> t -> t;;
+
+(** Get an element from a Bson document via its name *)
 val get_element : string -> t -> element;;
+
+(** Remove an element from a Bson document *)
 val remove_element : string -> t -> t;;
 
+(** Encode a Bson document to bytes (using type string as a carrier) *)
 val encode : t -> string;;
+
+(** Decode bytes (assuming type string as a carrier) to a Bson document *)
 val decode : string -> t;;
 
-val to_simple_json : t -> string;;
+
+(** {6 Creating elements} *)
+
 
 val create_double : float -> element;;
 val create_string : string -> element;;
@@ -38,6 +81,10 @@ val create_int32 : int32 -> element;;
 val create_int64 : int64 -> element;;
 val create_minkey : unit -> element;;
 val create_maxkey : unit -> element;;
+
+
+(** {6 Getting raw values from elements} *)
+
 
 val get_double : element -> float;;
 val get_string : element -> string;;
@@ -61,6 +108,11 @@ val get_timestamp : element -> int64;;
 val get_minkey : element -> empty;;
 val get_maxkey : element -> empty;;
 
+
+(** {6 Experimental. Convert a Bson document to Json.} *)
+
+
+val to_simple_json : t -> string;;
 
 (*val create_generic_binary : string -> element;;
     val create_function_binary : string -> element;;
