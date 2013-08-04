69213ba9da44fc43fa481c04b54bcd9715f54c53
Simon Marc <marc.simon42@gmail.com>
2013-08-03 20:50:56 -0700

    [fix] revert client lib to dummy plugin. Error when generating type_mli

--- client/src/bson_ext_client.ml
+++ client/src/bson_ext_client.ml
@@ -1,7 +1,5 @@
 module type Bson_ext = sig
   type a
-  val to_bson : a -> Bson_client.element
-  val from_bson : Bson_client.element -> a
 end
 
 module Default(D : Bson_ext) : Bson_ext with type a = D.a = struct
@@ -10,95 +8,39 @@ end
 
 module Bson_ext_int = Default(struct
     type a = int
-    let from_bson i = Int64.to_int (Bson_client.get_int64 i)
-
-    let to_bson i =
-      Bson_client.create_int64 (Int64.of_int i)
-
   end)
 
 
 module Bson_ext_int32 = Default(struct
     type a = int32
-
-    let from_bson = Bson_client.get_int32
-    let to_bson = Bson_client.create_int32
-
   end)
 
 module Bson_ext_int64 = Default(struct
     type a = int64
-
-    let from_bson = Bson_client.get_int64
-    let to_bson = Bson_client.create_int64
-
   end)
 
 module Bson_ext_bool = Default(struct
     type a = bool
-    let from_bson = Bson_client.get_boolean
-    let to_bson = Bson_client.create_boolean
   end)
 
 module Bson_ext_float = Default(struct
     type a = float
-
-    let from_bson = Bson_client.get_double
-    let to_bson = Bson_client.create_double
-
   end)
 
 module Bson_ext_string = Default(struct
     type a = string
-
-    let from_bson = Bson_client.get_string
-    let to_bson = Bson_client.create_string
-
   end)
 
 module Bson_ext_list (A : Bson_ext) = Default(struct
     type a = A.a list
-    let from_bson elt =
-      List.map (
-        fun bson ->
-          A.from_bson bson
-      ) (Bson_client.get_list elt)
-
-    let to_bson l =
-      Bson_client.create_list (List.map (fun e -> A.to_bson e) l)
-
   end)
 
 
 module Bson_ext_array (A : Bson_ext) = Default(struct
     type a = A.a array
-    let from_bson elt =
-      let l =
-        List.map (
-          fun bson ->
-            A.from_bson bson
-        ) (Bson_client.get_list elt)
-      in
-
-      Array.of_list l
-
-    let to_bson a =
-      let l = Array.to_list a in
-      Bson_client.create_list (List.map (fun e -> A.to_bson e) l)
   end)
 
 
 module Bson_ext_option (A : Bson_ext) = Default(struct
     type a = A.a option
-    let from_bson o =
-      try
-        let _ = Bson_client.get_null o in
-        None
-      with Bson_client.Wrong_bson_type ->
-        Some (A.from_bson o)
-
-    let to_bson = function
-      | None -> Bson_client.create_null ()
-      | Some o -> A.to_bson o
-
   end)
